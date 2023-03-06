c2ebade8562ac8c7c852a40ecd0f9c12dce09e80
navaneetha <navani91@gmail.com>
2023-03-06 16:18:30 +0000

    frontend.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,17 @@
+source common.sh
+
+print_head "setup mongodb repository"
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+print_head "install mongodb"
 yum install mongodb-org -y
+
+print_head "enable mongodb"
 systemctl enable mongod
+
+print_head "start monodb service"
 systemctl start mongod
+
 # update /etc/mongod.conf file from 127.0.0.1 with 0.0.0.0
 
 
